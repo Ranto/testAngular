1ccefed85e373518fc1076829a3550a5d1d16c2c
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2016-02-19 15:03:07 +0300

    Ajout service loading

---
+++ js/loading/loading.service.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+
+    var app = angular.module("loadingApp");
+
+    app.service('resourceService', resourceService);
+
+    function resourceService($http, $q) {
+        this.getPDF = function () {
+            var deferred = $q.defer();
+            $http.get('/resources/fiche.pdf')
+                .success( function(data, status) {
+                    deferred.resolve(data);
+                }).error( function(data, status) {
+                    deferred.reject('Impossible d\'avoir les données');
+                });
+
+            return deferred.promise;
+        };
+    }
+})();
