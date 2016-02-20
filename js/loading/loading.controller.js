474cd48c086f632fec82e021318cf4fa3c968a59
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2016-02-20 07:43:01 +0300

    Reception du fichier pdf à travers le service loadingService et affichage avec <embed> dans la vue

--- js/loading/loading.controller.js
+++ js/loading/loading.controller.js
@@ -3,9 +3,16 @@
     var app = angular.module("loadingApp");
 
     app.controller("loadingController", loadingController);
-    function loadingController($scope) {
+    function loadingController($scope, resourceService, $sce) {
         var vm = this;
+        vm.loading = true;
 
-        vm.data = "AAA";
+        resourceService.getPDF().then( function(dataResponse) {
+            vm.loading = false;
+            var data = new Blob([dataResponse.data], {type: "application/pdf"});
+            var url = URL.createObjectURL(data);
+
+            vm.surl = $sce.trustAsResourceUrl(url);
+        });
     }
 })();
