67ba79d4bb61d12351cff5c186538dc1541d8f27
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2016-03-02 15:39:10 +0300

    Ajout d'option sur la daterangepicker

--- js/daterange/dater.controller.js
+++ js/daterange/dater.controller.js
@@ -4,6 +4,27 @@
     angular.module("essaiApp").controller("daterController", function($scope) {
         $scope.dateRange = { startDate: null, endDate: null};
 
+        $scope.opts = {
+            locale: {
+                applyClass: 'btn-green',
+                applyLabel: "Valider",
+                fromLabel: "Du",
+                toLabel: "Au",
+                cancelLabel: 'Annuler',
+                customRangeLabel: 'Selection',
+                daysOfWeek: ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
+                firstDay: 1,
+                monthNames: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mais', 'Juin', 'Juillet', 'Aout', 'Septembre',
+                    'Octobre', 'Novembre', 'Décembre'
+                ],
+                format: 'DD/MM/YYYY'
+            },
+            ranges: {
+                '7 derniers jours': [moment().subtract(6, 'days'), moment()],
+                '30 derniers jours': [moment().subtract(29, 'days'), moment()]
+            }
+        };
+
 
     });
 })();
